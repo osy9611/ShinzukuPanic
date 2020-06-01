e5a8b47f6917a5d9ca3c37742c97b0a870f778ba
오섭균 <osy9611@gmail.com>
2020-06-02 00:06:12 +0900

    PooledObject  -오브젝트 풀링 구현 KatanaDamage  -근접 공격 데미지를 구현

---
+++ ShinzukuPanic/Assets/Script/Box/BoxDrop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DropTable
+{
+    public float Percentage;
+    public GameObject[] Item;
+}
+public class BoxDrop : MonoBehaviour {
+   
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
